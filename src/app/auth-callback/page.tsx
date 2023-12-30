c4f2f5051c1161ac086e65f47263aac38f62388d
Ayberk Uman <ayberkuman1@gmail.com>
2023-12-30 17:48:12 +0400

    feat: setup

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
+
+export default function Page() {
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const origin = searchParams.get("origin");
+  trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      if (success) {
+        // user is synced to db
+        console.log("success");
+        router.push(origin ? `/${origin}` : "/dashboard");
+      }
+    },
+    onError: (err) => {
+      if (err.data?.code === "UNAUTHORIZED") {
+        router.push("/sign-in");
+      }
+    },
+    retry: true,
+    retryDelay: 500,
+  });
+  return (
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <ReloadIcon className="w-8 h-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl ">Authenticating...</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  );
+}
